91c6ec9318624fe808047b316c3c81c26e473fa6
Pahud <pahudnet@gmail.com>
2020-10-06 12:40:37 +0800

    feat(cdk): add projen support

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+
+export class MyStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps = {}) {
+    super(scope, id, props);
+
+    const handler = new lambda.Function(this, 'LambdaHandler', {
+      code: lambda.Code.fromAsset('main.zip'),
+      handler: 'main',
+      runtime: lambda.Runtime.GO_1_X,
+    })
+
+    new apigateway.LambdaRestApi(this, 'API', {
+      handler
+    })
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new cdk.App();
+
+new MyStack(app, 'my-stack-dev', { env: devEnv });
+// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+
+app.synth();
