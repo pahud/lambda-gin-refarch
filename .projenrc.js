56d2ba214f949efdbd372c2b896fbc344d8a7c3a
Pahud Hsieh <pahudnet@gmail.com>
2021-06-01 10:33:46 +0800

    chore: bump versions and disable projen-automate-it (#17)
    * chore: bump versions and disable projen-automate-it
    
    * chore: self mutation
    
    Co-authored-by: Automation <github-actions@github.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,36 +1,23 @@
 const { AwsCdkTypeScriptApp } = require('projen');
-const { Automation } = require('projen-automate-it');
 
 const AUTOMATION_TOKEN = 'GITHUB_TOKEN';
 
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: "1.63.0",
-  name: "lambda-gin-refarch",
-  authorName: "Pahud Hsieh",
-  authorEmail: "pahudnet@gmail.com",
-  repository: "https://github.com/pahud/eks-lambda-py.git",
+  cdkVersion: '1.63.0',
+  name: 'lambda-gin-refarch',
+  authorName: 'Pahud Hsieh',
+  authorEmail: 'pahudnet@gmail.com',
+  repository: 'https://github.com/pahud/lambda-gin-refarchgit .git',
   dependabot: false,
   antitamper: false,
   defaultReleaseBranch: 'main',
   cdkDependencies: [
-    "@aws-cdk/core",
-    "@aws-cdk/aws-apigateway",
-    "@aws-cdk/aws-lambda",
-  ],
-    devDeps: [
-    'projen-automate-it',
+    '@aws-cdk/core',
+    '@aws-cdk/aws-apigateway',
+    '@aws-cdk/aws-lambda',
   ],
 });
-
-const automation = new Automation(project, {
-  automationToken: AUTOMATION_TOKEN,
-});
-
-automation.projenYarnUpgrade();
-automation.autoApprove();
-automation.autoMerge();
-
 
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'vendor', 'main', 'yarn-error.log'];
