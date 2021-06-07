54784446d217ff314d7017581c58fd15d6f64d1b
Pahud Hsieh <pahudnet@gmail.com>
2021-06-08 07:49:08 +0800

    bump projen (#20)

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,6 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
 
-const AUTOMATION_TOKEN = 'GITHUB_TOKEN';
-
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.63.0',
@@ -9,8 +8,16 @@ const project = new AwsCdkTypeScriptApp({
   authorName: 'Pahud Hsieh',
   authorEmail: 'pahudnet@gmail.com',
   repository: 'https://github.com/pahud/lambda-gin-refarchgit .git',
-  dependabot: false,
-  antitamper: false,
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
   defaultReleaseBranch: 'main',
   cdkDependencies: [
     '@aws-cdk/core',
@@ -18,7 +25,6 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-lambda',
   ],
 });
-
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'vendor', 'main', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);
